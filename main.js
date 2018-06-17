5e13b919dea6feb6d35034da2ff63922900c59a5
Jos <josvanuden@gmail.com>
2018-06-17 13:15:05 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+function permutations(s) {
+    const result = [];
+
+    function permute(arr, size) {
+
+        function swap(idx1, idx2) {
+            const tmp = arr[idx1];
+            arr[idx1] = arr[idx2];
+            arr[idx2] = tmp;
+        }
+
+        if (size == 1) {
+            result.push(arr.slice());
+            return;
+        }
+
+        for (let i = 0; i < size; i++) {
+
+            permute(arr, size - 1);
+
+            if (size & 1)
+                swap(0, size - 1);
+            else
+                swap(i, size - 1);
+        }
+    }
+    permute(s.split(''), s.length);
+
+    return [...new Set(result.map(a => a.join('')))];
+}
